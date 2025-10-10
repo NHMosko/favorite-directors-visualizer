4a675b77e7f6f4abbbab72a905716ec07919c6a9
NHMosko <nick.mosko11@gmail.com>
2025-10-10 13:56:43 -0300

    start adding letterboxd support

--- app.js
+++ app.js
@@ -27,10 +27,26 @@
         reader.onload = function (evt) {
             let csvString = evt.target.result;
             csvArray = parseToArray(csvString);
-            csvArray.shift();
-            for (row of csvArray) {
-                rawArray.push([row[RATING_IDX], extractDirectors(row)]);
-            }
+            let headers = csvArray.shift();
+			if (headers[0].startsWith("Const")) {
+				console.log("IMDb")
+				for (row of csvArray) {
+					rawArray.push([row[RATING_IDX], extractDirectors(row)]);
+				}
+			} else if (headers[0].startsWith("Date")) {
+				console.log("Letterboxd")
+				for (row of csvArray) {
+					if (row.length > 1) {
+						rawArray.push([row[row.length - 1], getDirectorFromTMDb(row)]);
+					}
+				}
+				console.log(rawArray)
+				return
+			} else {
+				console.log("Unsupported file")
+				return
+			}
+
             for (row of rawArray) {
                 let rating = parseInt(row[0])
                 let directors = row[1]
@@ -58,7 +74,7 @@
     function parseToArray(csvString){
         //Split the array into rows, then split these rows into cells
         return csvString.split('\n').map(row => {
-            return row.split(',')
+            return row.trim().split(',')
         })
     }
 
@@ -72,6 +88,35 @@
             }
         }
     }
+
+	function getDirectorFromTMDb(row) {
+		let titleAndYear = []
+		if (row[1][0] != "\"") {
+			titleAndYear = [row[1], row[2]]
+		} else {
+			titleAndYear = extractTitleAndYear(row)
+		}
+		//console.log(titleAndYear)
+		return titleAndYear
+	}
+
+	function extractTitleAndYear(row) {
+		let title = ""
+		let year = 0
+
+		for (let i = 1; i < row.length; i++) {
+			//console.log(row[i][row[i].length - 1])
+			if (row[i][row[i].length - 1] == "\"") {
+				title += row[i]
+				year = row[i + 1]
+				break
+			}
+
+			title += row[i] + ","
+		}
+
+		return [title, year]
+	}
 
     function convertMapToArray(directorMap) {
         let out = [];
@@ -153,15 +198,34 @@
             let va = a[sortKey], vb = b[sortKey];
 
             const na = parseFloat(va), nb = parseFloat(vb);
+			let cmp;
             if (!isNaN(na) && !isNaN(nb)) {
-                return ascending ? na - nb : nb - na;
-            }
+                cmp = ascending ? na - nb : nb - na;
+            } else {
+				va = (va ?? '').toString().toLowerCase();
+				vb = (vb ?? '').toString().toLowerCase();
+				if (va < vb) {
+					cmp = ascending ? -1 : 1;
+				} else if (va > vb) {
+					cmp = ascending ? 1 : -1;
+				} else {
+					cmp = 0
+				}
+			}
 
-            va = (va ?? '').toString().toLowerCase();
-            vb = (vb ?? '').toString().toLowerCase();
-            if (va < vb) return ascending ? -1 : 1;
-            if (va > vb) return ascending ? 1 : -1;
-            return 0;
+			if (cmp === 0) {
+				if (sortKey === "movie_count") {
+					const rateA = parseFloat(a.avg_rating)
+					const rateB = parseFloat(b.avg_rating)
+					cmp = ascending ? rateA - rateB : rateB - rateA
+				} else {
+					const countA = parseFloat(a.movie_count)
+					const countB = parseFloat(b.movie_count)
+					cmp = ascending ? countA - countB : countB - countA
+				}
+			}
+
+            return cmp;
         });
         buildTable(sorted);
     }
